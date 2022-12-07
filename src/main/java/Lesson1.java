3dd463fe2cd8b8df7e9a9c0113163d714aa05749
StanislavPB <perminovsb@gmail.com>
2022-12-07 18:18:59 +0200

    lesson1

---
+++ src/main/java/Lesson1.java
@@ -0,0 +1,13 @@
+public class Lesson1 {
+
+    public static void main(String[] args) {
+
+        String t1 = "Hello, World!";
+
+        System.out.println(t1);
+
+        t1= "Hello!!!";
+
+        System.out.println(t1);
+    }
+}
---
+++ src/main/java/LessonVarExample.java
@@ -0,0 +1,49 @@
+public class LessonVarExample {
+
+    public static void main(String[] args) {
+
+        // Объявляем переменные
+        int x = 25;
+
+        int y = x;
+
+        int z = x + y;
+
+        /*
+        Comment 1
+        Comment 2
+        Comment 3
+         */
+
+        double pi = 3.14151;
+
+        double result = x / 5.0;
+
+        // переменная может иметь только два значения true или false
+        boolean check;
+
+        check = (x == y);
+
+        boolean b1 = true;
+
+        boolean b2 = false;
+
+        System.out.println(b1 & b2);
+        System.out.println(b1 || b2);
+        System.out.println(!b1);
+
+
+        int a = 12;
+        int b = 4;
+
+        double res = a / b;
+
+        System.out.println(res);
+
+
+        System.out.println("X = " + x);
+        System.out.println("Result = " + result);
+        System.out.println("Check is - " + check);
+
+    }
+}
